bb9cfaecd0135f0fc4f175ba1ae0df09efa6a45d
dawid.wawryka <dawid.wawryka@gmail.com>
2018-08-11 18:43:01 +0200

    Improvements for conversion from html to pdf

--- src/main/java/FileUtils.java
+++ src/main/java/FileUtils.java
@@ -1,7 +1,13 @@
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.PrintWriter;
-import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.tool.xml.exceptions.RuntimeWorkerException;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 
 class FileUtils {
 
@@ -10,11 +16,39 @@ class FileUtils {
         return new String(encoded, "UTF-8");
     }
 
-    public static void saveFile(String fileContent, String fileName) {
-        try(PrintWriter out = new PrintWriter(fileName)) {
+    public static void saveFile(String fileContent, String filePath) {
+        try(PrintWriter out = new PrintWriter(filePath)) {
             out.println(fileContent);
         } catch(IOException e) {
-            System.out.println("Couldn't save file '" + fileName + "': " + e.toString());
+            System.out.println("Couldn't save file '" + filePath + "': " + e.toString());
+        }
+    }
+
+    public static void html2Pdf(String inFilePath, String outFilePath) {
+
+        Document doc = new Document();
+        try {
+            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream(outFilePath));
+            doc.open();
+            XMLWorkerHelper.getInstance().parseXHtml(writer, doc, new FileInputStream(inFilePath));
+        } catch(DocumentException | RuntimeWorkerException | IOException e) {
+            System.out.println("ERROR Couldn't save PDF file '" + outFilePath + "': " + e.toString());
+        } finally {
+            if(doc.isOpen()) {
+                doc.close();
+            }
+        }
+    }
+
+    public static void openhtmltopdf(String inFilePath, String outFilePath) {
+
+        try(FileOutputStream fos = new FileOutputStream(outFilePath)) {
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.withFile(new File(inFilePath));
+            builder.toStream(fos);
+            builder.run();
+        } catch(Exception e) {
+            System.out.println("ERROR Couldn't save PDF file '" + outFilePath + "': " + e.toString());
         }
     }
 }
